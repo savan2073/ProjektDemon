cf1bb335bf8f11ef2e8065ff0305c10099ec4302
Test User <savan2073@gmail.com>
2022-04-12 22:19:05 +0200

    help add

---
+++ help.c
@@ -0,0 +1,11 @@
+#include "help.h"
+#include <stdio.h>
+
+void help(bool full){
+
+
+    if(!full){
+        "\nDisplay help: demon -h\n";
+        return;
+    }
+}
---
+++ help.h
@@ -0,0 +1,13 @@
+#ifndef HELP_H
+#define HELP_H
+
+#include <stdbool.h>
+
+void help(bool full);
+
+void help_parametry();
+
+void help_actions();
+
+
+#endif
