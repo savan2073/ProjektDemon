b3a044497ca476ec6cc8ddf7a4faf4c21365c855
Test User <savan2073@gmail.com>
2022-05-01 18:30:20 +0200

    st_mtime atime

--- timestamp.c
+++ timestamp.c
@@ -7,8 +7,8 @@ void clone_timestamp(char* source_file, char* destination_file){
     struct stat st;
     struct utimbuf new_time;
     stat(source_file, &st);
-    new_time.actime = st.st_atim.tv_sec;// fking st_atim doesn't work
-    new_time.modtime = st.st_mtim.tv_sec;// fking st_mtim doesn't work
+    new_time.actime = st.st_atime;// fking st_atim doesn't work
+    new_time.modtime = st.st_mtime;// fking st_mtim doesn't work
     utime(destination_file, &new_time);
     chmod(destination_file, st.st_mode);
     
@@ -19,6 +19,6 @@ bool compare_timestamp(char* source_file, char* destination_file){
     struct stat st2;
     stat(source_file, &st1);
     stat(destination_file, &st2);
-    return (st1.st_mtim.tv_sec == st2.st_mtim.tv_sec);
+    return (st1.st_mtime == st2.st_mtime);
 
 }
