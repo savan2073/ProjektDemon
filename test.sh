075f032a06e1babeb84be6ae1eb2e8f08eac7104
Test User <savan2073@gmail.com>
2022-05-01 19:01:46 +0200

    starter

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ mkdir ~/source/a ~/source/b ~/source/b/bb ~/source/c
 touch ~/source/file1 ~/source/a/file2 ~/source/b/file3
 
 echo "Demon start with recursive function every 30 seconds"
-demon -R -s 10 ~/source ~/destination
+./demon -R -s 10 ~/source ~/destination
 
 echo "Compare source directory with destination directory"
 diff ~/source ~/destination
